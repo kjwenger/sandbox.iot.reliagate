c254db2207e54797ac791797a8b5a776448f8722
Klaus Wenger <kjwenger@yahoo.com>
2018-05-04 12:17:42 +0200

    initial load

---
+++ script/build.arm-poky-linux-gnueabi.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#set -x
+
+CPUS=$(lscpu | grep "^CPU(s):" | sed s/"CPU(s):                "//)
+
+MULTIARCH_TUPLE="arm-poky-linux-gnueabi"
+
+CURRENT_DIR="$(pwd)"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(dirname "${SCRIPTS_DIR}")"
+STAGING_DIR="${PROJECT_DIR}/staging/${MULTIARCH_TUPLE}"
+BUILD_DIR="${PROJECT_DIR}/build.${MULTIARCH_TUPLE}"
+
+cd "${PROJECT_DIR}"
+mkdir -p "${BUILD_DIR}"
+pushd "${BUILD_DIR}"
+rm -Rf *
+cmake \
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      -DCMAKE_TOOLCHAIN_FILE="${PROJECT_DIR}/toolchain/${MULTIARCH_TUPLE}.cmake" \
+      -DCMAKE_INSTALL_PREFIX="${STAGING_DIR}" \
+      ..
+make -j ${CPUS}
+popd
+cd "${CURRENT_DIR}"
